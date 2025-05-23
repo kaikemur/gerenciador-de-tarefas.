f359a4b0b59bd69610a8df03c6b76716066af6e3
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-23 11:20:29 -0300

    alteração de ARQUIVO

--- aulas 23-05/aula-10 map.js
+++ aulas 23-05/aula-10 map.js
@@ -2,3 +2,26 @@ let playlist =[`funk`,`hip-hop`,`pop`]
 let playlistEmcaps = playlist.map(musica => musica.toUpperCase());
 
 console.log("playlist em maiusculas:", playlistEmcaps);
+
+console.log(`-----------------------------------------------------`);
+
+let minuscula= playlist.map(musica => musica.toLowerCase());
+console.log(`misuculas com map(): ${minuscula.join(`, `)}`);
+
+console.log(`-----------------------------------------------------`);
+
+
+let numeros = [10,20,30,40,50];
+
+let dobrados = numeros.map(n => n * 2);
+console.log(`valores dobrados: ${dobrados.join(`, `)}`);
+
+console.log(`-----------------------------------------------------`);
+
+let metades = numeros.map(n => n / 2);
+console.log(`valores pelametades: ${metades.join(`, `)}`);
+
+console.log(`-----------------------------------------------------`);
+
+let comoTexto = numeros.map(n => `${n} pts `);
+console.log(`como texto: ${comoTexto.join(`, `)}`);
