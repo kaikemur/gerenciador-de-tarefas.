bd9b2e9c13aa0c2ce7b298c8953b9e497f34b245
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-23 09:38:03 -0300

    adiciona doisnovos arqquivos

---
+++ aula-05 iclude.js
@@ -0,0 +1,5 @@
+let playlist = [`funk`,`hip-hop`,`opera`,`hard-rock`,`ninar `,`jazz`,`pop`]
+console.log("A playlist inclui funk`?" , playlist.includes(`funk`));
+
+
+console.log(`A playlist inclui a palavra pop ? ${playlist.includes("pop")}`);
---
+++ aula-06 join.js
@@ -0,0 +1,4 @@
+let playlist = [`funk`,`hip-hop`,`pop`,`trap`];
+
+console.log(`playlist como string : ${playlist.join(`, `)}`);
+
