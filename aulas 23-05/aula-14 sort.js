79cfd354eaa017643a14f470976ca24857eb0308
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-23 11:26:05 -0300

    adiciona arquivos novos

---
+++ aulas 23-05/aula-14 sort.js
@@ -0,0 +1,15 @@
+let numeros = [10, 5, 100, 50, 1];
+
+console.log("Antes do sort:", numeros);
+
+// Ordenando os números de forma crescente
+
+let numerosOrdenadosCrescentes = numeros.sort((a, b) => a - b);
+
+console.log("Números em ordem crescente:", numerosOrdenadosCrescentes);
+
+// Ordenando os números de forma decrescente
+
+let numerosOrdenadosDecrescentes = numeros.sort((a, b) => b - a);
+
+console.log("Números em ordem decrescente:", numerosOrdenadosDecrescentes);
