a1c8dbde911677c66319382bc094ef959a87ef39
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-23 10:51:45 -0300

    adiciona novo arquivo

---
+++ aulas 23-05/aula-09 concat.js
@@ -0,0 +1,5 @@
+let playlist1 = [`funk`,`hip-hop`,`pop`];
+let playlist2 = [`rock`,`jazz`]
+
+console.log(`playlist combinadA: ${playlist1.concat(playlist2)}`);
+console.log(`playlist combinadA: ${playlist1.concat(playlist2).join(`, `)}`);
