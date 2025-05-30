33b977c2d1061f053b3b7eeee60ab5a74893d7f1
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 08:37:14 -0300

    adiciona novas tarefa

---
+++ atividade-final.js
@@ -0,0 +1,7 @@
+
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+tarefas.push(`programar`,`limpar o  quintal`);
+
+console.log(`depois do push`, tarefas);
+git 
