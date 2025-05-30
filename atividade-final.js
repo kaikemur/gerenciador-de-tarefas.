187746c210ba3e396ba4cb14d8acfce9ac2d3d2d
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 08:50:52 -0300

    remove a ultima tarefa

--- atividade-final.js
+++ atividade-final.js
@@ -1,7 +1,28 @@
+//Etapa 1
+let tarefas = [
+  "Estudar JavaScript",
+  "Fazer exercícios de matemática",
+  "Lavar a louça",
+  "Comprar mercado",
+  "Ir à academia",
+  "Ler um livro",
+  "Fazer revisão para o teste",
+  "Limpar o quarto",
+  "Estudar para concurso",
+  "Organizar o computador",
+];
 
-let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+console.log(`antes do push`, tarefas);
 
-tarefas.push(`programar`,`limpar o  quintal`);
+tarefas.push(`programar`, `limpar o  quintal`);
 
 console.log(`depois do push`, tarefas);
-git 
+console.log(`---------------------------------------------------------------------------`);
+//Etapa 2
+
+let tarefaRemovida = tarefas.pop();
+console.log(`array com o pop`, tarefas);
+console.log(` tarefa Removida:`, tarefaRemovida);
+
+console.log(`---------------------------------------------------------------------------`);
+
